bbd7ec677be59cf06f5d066cb5c66fee085347d9
StillMix <stillmixplaygames@gmail.com>
2025-06-19 22:55:56 +0300

    db new

--- server.js
+++ server.js
@@ -18,19 +18,33 @@ const db = new sqlite3.Database(dbPath, (err) => {
 })
 
 function initDB() {
+
     db.run(
-        `CREATE TABLE users (
+        `CREATE TABLE IF NOT EXISTS users (
         userID INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL
+        name TEXT NOT NULL,
         password TEXT NOT NULL
         )`, (err) => {
             if(err) {
                 console.log(err)
             } else {
-                console.log('Таблица создана')
+                console.log('Таблица users создана')
             }
         }
-    )
+    );
+    
+    db.run(
+        `CREATE TABLE IF NOT EXISTS post (
+        postID INTEGER PRIMARY KEY AUTOINCREMENT,
+        post TEXT NOT NULL
+        )`, (err) => {
+            if(err) {
+                console.log(err)
+            } else {
+                console.log('Таблица post создана')
+            }
+        }
+    );
 }
 
 app.post('/user', (req, res) => {
@@ -70,6 +84,48 @@ app.get('/user/:id', (req, res) => {
         })
     }
 })
+
+
+app.post('/post', (req, res) => {
+    const post = req.body
+    if (!post.post){
+        res.status(400).json({ error: 'Должен содержать текст поста!' })
+    } else{
+        db.run('INSERT INTO post (post) VALUES (?)', [post.post], (err) => {
+            if(err){
+                console.log(err)
+                res.status(500).json({ error: 'Ошибка базы данных' })
+            }else{
+                res.status(201).json({ message: 'Пост создан' })
+            }
+        })
+    }
+})
+
+app.get('/post/:id', (req, res) => {
+    const id = req.params.id
+    if(!id){
+        res.status(400).json({ error: 'ID не указан' })
+    }else{
+        db.get(`SELECT * FROM post WHERE postID = ?`, [id], (err, row) => {
+            if(err){
+                console.log(err)
+                res.status(500).json({ error: 'Ошибка базы данных' })
+            }else if(!row){
+                res.status(404).json({ error: 'Пост не найден' })
+            }else{
+                res.status(200).json({
+                    message: 'Пост найден',
+                    post: row
+                })
+            }
+        })
+    }
+})
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`)
